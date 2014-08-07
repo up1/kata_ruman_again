7d6c81246d878aac9852721fd1e3da6eeb9f513f
somkiat <somkiat.p@gmail.com>
2014-08-07 10:17:00 +0700

    Resolve second test with Hashmap and one if => it's ok

--- src/main/java/dojo/roman/numeric/noif/Roman.java
+++ src/main/java/dojo/roman/numeric/noif/Roman.java
@@ -1,11 +1,22 @@
 package dojo.roman.numeric.noif;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Roman {
 
 	public String convert(int arabicNumber) {
+		Map<Integer, String> arabicToRomanMap = new HashMap<Integer, String>();
+		arabicToRomanMap.put(4, "IV");
+
 		String romanResult = "";
-		for(int start=0; start<arabicNumber; start++) {
-			romanResult += "I";
+		for (int start = 0; start < arabicNumber; start++) {
+			if (arabicToRomanMap.containsKey(arabicNumber)) {
+				romanResult += arabicToRomanMap.get(arabicNumber);
+				arabicNumber -= arabicNumber;
+			} else {
+				romanResult += "I";
+			}
 		}
 		return romanResult;
 	}
